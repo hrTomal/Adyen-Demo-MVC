0777548b058b7218e9015b6e09a4413f0d79c5cf
Hasibur Rahman Tomal <hasibur.rahman@JustGo.com>
2024-05-21 13:18:50 +0600

    coutries added

---
+++ Adyen-Payment-Gateway-Demo-MVC/Controllers/StaticDataController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Adyen.Model.Checkout;
+using Adyen_Payment_Gateway_Demo_MVC.Application.Features.AdyenPayments.AdyenLibrary;
+using Newtonsoft.Json;
+
+namespace Adyen_Payment_Gateway_Demo_MVC.Controllers
+{
+    [RoutePrefix("api/static")]
+    public class StaticDataController : Controller
+    {
+
+        [HttpGet]
+        [Route("countries")]
+        public async Task<ActionResult> Countries()
+        {
+            var countries = new List<Dictionary<string, string>>
+            {
+                new Dictionary<string, string> { { "label", "Australia" }, { "value", "AU" } },
+                new Dictionary<string, string> { { "label", "Hong Kong" }, { "value", "HK" } },
+                new Dictionary<string, string> { { "label", "India" }, { "value", "IN" } },
+                new Dictionary<string, string> { { "label", "European Union" }, { "value", "EU" } },
+                new Dictionary<string, string> { { "label", "United Kingdom" }, { "value", "UK" } },
+                new Dictionary<string, string> { { "label", "United States" }, { "value", "US" } }
+            };
+
+            return Content(JsonConvert.SerializeObject(countries), "application/json");
+        }
+    }
+}
